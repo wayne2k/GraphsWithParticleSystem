e116e947c8c2def97552f612ec2e17e1e783d2bd
wayne2k <wayne2k@gmail.com>
2015-02-27 16:45:46 +0200

    3D effects

--- Assets/Grapher3.cs
+++ Assets/Grapher3.cs
@@ -12,7 +12,7 @@ public class Grapher3 : MonoBehaviour {
 	}
 	
 	public FunctionOption function;
-	[Range (0, 100)]
+	[Range (10, 30)]
 	public int resolution = 10;
 
 	private int currentResolution = 0;
@@ -28,15 +28,17 @@ public class Grapher3 : MonoBehaviour {
 
 	private void CreatePoints () {
 		currentResolution = resolution;
-		points = new ParticleSystem.Particle[resolution * resolution];
+		points = new ParticleSystem.Particle[resolution * resolution * resolution];
 		float increment = 1f / (resolution - 1);
 		int i = 0;
 		for (int x = 0; x < resolution; x++) {
 			for (int z = 0; z < resolution; z++) {
-				Vector3 p = new Vector3(x * increment, 0f, z * increment);
-				points[i].position = p;
-				points[i].color = new Color(p.x, 0f, p.z);
-				points[i++].size = 0.1f;
+				for (int y = 0; y < resolution; y++) {
+					Vector3 p = new Vector3(x, y, z) * increment;
+					points[i].position = p;
+					points[i].color = new Color(p.x, p.y, p.z);
+					points[i++].size = 0.1f;
+				}
 			}
 		}
 	}
@@ -48,41 +50,39 @@ public class Grapher3 : MonoBehaviour {
 		FunctionDelegate f = functionDelegates[(int)function];
 		float t = Time.timeSinceLevelLoad;
 		for (int i = 0; i < points.Length; i++) {
-			Vector3 p = points[i].position;
-			p.y = f(p,t);
-			points[i].position = p;
 			Color c = points[i].color;
-			c.g = p.y;
+			c.a = f(points[i].position, t);
 			points[i].color = c;
 		}
 		particleSystem.SetParticles(points, points.Length);
 	}
 
 	private static float Linear (Vector3 p, float t) {
-		return p.x;
+		return 1f - p.x - p.y - p.z + 0.5f * Mathf.Sin(t);
 	}
 
 	private static float Exponential (Vector3 p, float t) {
-		return p.x * p.x;
+		return 1f - p.x * p.x - p.y * p.y - p.z * p.z + 0.5f * Mathf.Sin(t);
 	}
 	
 	private static float Parabola (Vector3 p, float t){
 		p.x += p.x - 1f;
 		p.z += p.z - 1f;
-		return 1f - p.x * p.x * p.z * p.z;
+		return 1f - p.x * p.x - p.z * p.z + 0.5f * Mathf.Sin(t);
 	}
 	
 	private static float Sine (Vector3 p, float t){
-		return 0.50f +
-				0.25f * Mathf.Sin(4f * Mathf.PI * p.x + 4f * t) * Mathf.Sin(2f * Mathf.PI * p.z + t) +
-				0.10f * Mathf.Cos(3f * Mathf.PI * p.x + 5f * t) * Mathf.Cos(5f * Mathf.PI * p.z + 3f * t) +
-				0.15f * Mathf.Sin(Mathf.PI * p.x + 0.6f * t);
+		float x = Mathf.Sin(2 * Mathf.PI * p.x);
+		float y = Mathf.Sin(2 * Mathf.PI * p.y);
+		float z = Mathf.Sin(2 * Mathf.PI * p.z + (p.y > 0.5f ? t : -t));
+		return x * x * y * y * z * z;
 	}
 
 	private static float Ripple (Vector3 p, float t){
 		p.x -= 0.5f;
+		p.y -= 0.5f;
 		p.z -= 0.5f;
-		float squareRadius = p.x * p.x + p.z * p.z;
-		return 0.5f + Mathf.Sin(15f * Mathf.PI * squareRadius - 2f * t) / (2f + 100f * squareRadius);
+		float squareRadius = p.x * p.x + p.y * p.y + p.z * p.z;
+		return Mathf.Sin(4f * Mathf.PI * squareRadius - 2f * t);
 	}
 }
