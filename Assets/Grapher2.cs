ddd9bbd1f0d51d78436ba89fc8d828f5798d9ee0
wayne2k <wayne2k@gmail.com>
2015-02-27 16:06:13 +0200

    Updated Functions Sine, parabola. + Ripple Added
    Oscallitating Sine
    Parabola changed
    Ripple function (a sine emits from center)

--- Assets/Grapher2.cs
+++ Assets/Grapher2.cs
@@ -7,7 +7,8 @@ public class Grapher2 : MonoBehaviour {
 		Linear,
 		Exponential,
 		Parabola,
-		Sine
+		Sine,
+		Ripple
 	}
 	
 	public FunctionOption function;
@@ -16,12 +17,13 @@ public class Grapher2 : MonoBehaviour {
 
 	private int currentResolution = 0;
 	private ParticleSystem.Particle[] points;
-	private delegate float FunctionDelegate (float x);
+	private delegate float FunctionDelegate (Vector3 p, float t);
 	private static FunctionDelegate[] functionDelegates = {
 		Linear,
 		Exponential,
 		Parabola,
-		Sine
+		Sine,
+		Ripple
 	};
 
 	private void CreatePoints () {
@@ -44,9 +46,10 @@ public class Grapher2 : MonoBehaviour {
 			CreatePoints();
 		}
 		FunctionDelegate f = functionDelegates[(int)function];
+		float t = Time.timeSinceLevelLoad;
 		for (int i = 0; i < points.Length; i++) {
 			Vector3 p = points[i].position;
-			p.y = f(p.x);
+			p.y = f(p,t);
 			points[i].position = p;
 			Color c = points[i].color;
 			c.g = p.y;
@@ -55,20 +58,31 @@ public class Grapher2 : MonoBehaviour {
 		particleSystem.SetParticles(points, points.Length);
 	}
 
-	private static float Linear (float x) {
-		return x;
+	private static float Linear (Vector3 p, float t) {
+		return p.x;
 	}
 
-	private static float Exponential (float x) {
-		return x * x;
+	private static float Exponential (Vector3 p, float t) {
+		return p.x * p.x;
 	}
 	
-	private static float Parabola (float x){
-		x = 2f * x - 1f;
-		return x * x;
+	private static float Parabola (Vector3 p, float t){
+		p.x += p.x - 1f;
+		p.z += p.z - 1f;
+		return 1f - p.x * p.x * p.z * p.z;
 	}
 	
-	private static float Sine (float x){
-		return 0.5f + 0.5f * Mathf.Sin(2 * Mathf.PI * x + Time.timeSinceLevelLoad);
+	private static float Sine (Vector3 p, float t){
+		return 0.50f +
+				0.25f * Mathf.Sin(4f * Mathf.PI * p.x + 4f * t) * Mathf.Sin(2f * Mathf.PI * p.z + t) +
+				0.10f * Mathf.Cos(3f * Mathf.PI * p.x + 5f * t) * Mathf.Cos(5f * Mathf.PI * p.z + 3f * t) +
+				0.15f * Mathf.Sin(Mathf.PI * p.x + 0.6f * t);
+	}
+
+	private static float Ripple (Vector3 p, float t){
+		p.x -= 0.5f;
+		p.z -= 0.5f;
+		float squareRadius = p.x * p.x + p.z * p.z;
+		return 0.5f + Mathf.Sin(15f * Mathf.PI * squareRadius - 2f * t) / (2f + 100f * squareRadius);
 	}
 }
