1e8c4b27df20e408235fc4af66fa0c46b49a51a7
wayne2k <wayne2k@gmail.com>
2015-02-27 15:32:23 +0200

    Showing multiple graphs
    Part 1

--- Assets/Grapher1.cs
+++ Assets/Grapher1.cs
@@ -3,12 +3,27 @@ using System.Collections;
 
 public class Grapher1 : MonoBehaviour {
 
+	public enum FunctionOption {
+		Linear,
+		Exponential,
+		Parabola,
+		Sine
+	}
+	
+	public FunctionOption function;
 	[Range (0, 100)]
 	public int resolution = 10;
 
 	private int currentResolution = 0;
 	private ParticleSystem.Particle[] points;
-	
+	private delegate float FunctionDelegate (float x);
+	private static FunctionDelegate[] functionDelegates = {
+		Linear,
+		Exponential,
+		Parabola,
+		Sine
+	};
+
 	private void CreatePoints () {
 		currentResolution = resolution;
 		points = new ParticleSystem.Particle[resolution];
@@ -25,14 +40,32 @@ public class Grapher1 : MonoBehaviour {
 		if (currentResolution != resolution || points == null) {
 			CreatePoints();
 		}
+		FunctionDelegate f = functionDelegates[(int)function];
 		for (int i = 0; i < resolution; i++) {
 			Vector3 p = points[i].position;
-			p.y = p.x;
+			p.y = f(p.x);
 			points[i].position = p;
 			Color c = points[i].color;
 			c.g = p.y;
 			points[i].color = c;
 		}
 		particleSystem.SetParticles(points, points.Length);
+	}
+
+	private static float Linear (float x) {
+		return x;
+	}
+
+	private static float Exponential (float x) {
+		return x * x;
+	}
+	
+	private static float Parabola (float x){
+		x = 2f * x - 1f;
+		return x * x;
+	}
+	
+	private static float Sine (float x){
+		return 0.5f + 0.5f * Mathf.Sin(2 * Mathf.PI * x);
 	}
 }
